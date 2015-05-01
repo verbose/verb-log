adaab057c48f585294d616f8f69c5d18904d49b4
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-01 05:10:38 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/*!
+ * verb-log <https://github.com/jonschlinkert/verb-log>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+var chalk = require('chalk');
+var timestamp = require('time-stamp');
+
+module.exports = function log() {
+  var time = '[' + chalk.grey(timestamp()) + ']';
+  var args = [time].concat([].slice.call(arguments));
+  console.log.apply(console, args);
+  return this;
+};
