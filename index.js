8bb85ef4f55d8f17f084a70471b9e551ae4fa7bb
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-27 22:36:25 -0400

    change pattern to hours:min:sec

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var chalk = require('chalk');
 var timestamp = require('time-stamp');
 
 module.exports = function log() {
-  var time = '[' + chalk.grey(timestamp()) + ']';
+  var time = '[' + chalk.grey(timestamp('HH:mm:ss')) + ']';
   var args = [time].concat([].slice.call(arguments));
   console.log.apply(console, args);
   return this;
